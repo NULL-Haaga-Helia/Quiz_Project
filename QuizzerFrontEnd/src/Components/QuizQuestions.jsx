51e89dcd032fe43d9a38b473b4a48e564e1ad28c
hxrda <henein.roda@gmail.com>
2024-11-25 07:21:18 +0200

    small modification

--- QuizzerFrontEnd/src/Components/QuizQuestions.jsx
+++ QuizzerFrontEnd/src/Components/QuizQuestions.jsx
@@ -1,28 +1,43 @@
 import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import { getQuizQuestions } from "../services/api";
+import { getQuizById, getQuizQuestions } from "../services/api";
 
 import Typography from "@mui/material/Typography";
 import { Box, List, ListItem, ListItemText, Paper } from "@mui/material";
 
 function QuizQuestions() {
 	//States:
+	const [quiz, setQuiz] = useState(null);
 	const [questions, setQuestions] = useState([]);
 	const location = useLocation();
-	// Access passed state / quiz object
-	const { quiz } = location.state;
+	//const { quizId } = location.state;
+	const quizId = location.state?.quizId;
+
+	// Tests with whole obj state.
+	//const { quiz } = location.state;
 	//const { state } = useLocation();
 	//const quiz = state?.quiz;
 
 	//Functions:
 	useEffect(() => {
-		if (quiz) {
+		if (quizId) {
+			fetchQuiz();
 			fetchQuizQuestions();
 		}
-	}, []);
+	}, []); //or  [quizId] to run when quizId changes?
+
+	const fetchQuiz = () => {
+		getQuizById(quizId)
+			.then((responseData) => {
+				console.log("Fetched quiz:", responseData);
+				setQuiz(responseData);
+			})
+			.catch((err) => console.error("Failed to fetch quiz:", err));
+		//Probably redundant, optionally show an error msg in the UI.
+	};
 
 	const fetchQuizQuestions = () => {
-		getQuizQuestions(quiz.id)
+		getQuizQuestions(quizId)
 			.then((responseData) => {
 				console.log("Fetched questions:", responseData);
 				setQuestions(responseData);
@@ -32,11 +47,18 @@ function QuizQuestions() {
 	};
 
 	// Rendering:
-	if (!quiz) {
-		console.warn("Quiz data unavailable");
+	if (!quizId) {
+		console.warn("Quiz ID is unavailable");
 		return (
 			<Box sx={{ width: "100%", marginTop: 8 }}>
-				<Typography variant="h5">Quiz data unavailable</Typography>
+				<Typography variant="h5">Quiz ID is unavailable</Typography>
+			</Box>
+		);
+	}
+	if (!quiz) {
+		return (
+			<Box sx={{ width: "100%", marginTop: 8 }}>
+				<Typography variant="h5">Loading quiz ...</Typography>
 			</Box>
 		);
 	} else {
@@ -52,7 +74,7 @@ function QuizQuestions() {
 					{quiz.description}
 				</Typography>
 
-				{/* Additional Information */}
+				{/* Additional Details */}
 				<Typography variant="body2" color="textSecondary" gutterBottom>
 					Added on: {quiz.addedOn} | Questions: {questions.length} | Category:{" "}
 					{quiz.quizCategory.name}
